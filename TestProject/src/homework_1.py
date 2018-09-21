1b75606599400c69dfa2380cd5608e6fdb442d0b
dror@columbus <dror.guzman@jhexagon.com>
2018-09-21 14:30:11 -0400

    HW 1 assigned.

---
+++ TestProject/src/homework_1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+print ("welcome to your first HW: FILTER BY")
+
+df = pd.DataFrame({"ID":[703,783,788,780,790],"NAME":["John","James","Marry","Gil","Roy"], "LEVEL":[5,5,4,5,4],"GRADE":[87,88,90,65,100]})
+
+print (df)
+print ("--------------")
+print ("Your task: given the dataframe above print on screen all those that their grade is grater than 80")

ab23bb92d59f68ad1b9594aed6bda219397172c6
columbus <=dror.guzman@jhexagon.com>
2018-10-20 10:16:32 -0400

    lets do it

--- TestProject/src/homework_1.py
+++ TestProject/src/homework_1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-print ("welcome to your first HW: FILTER BY")
+print ("welcome to your first HW: FILTER BY. So yallaa...")
 
 df = pd.DataFrame({"ID":[703,783,788,780,790],"NAME":["John","James","Marry","Gil","Roy"], "LEVEL":[5,5,4,5,4],"GRADE":[87,88,90,65,100]})
 
